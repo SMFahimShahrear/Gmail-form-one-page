ccd9c3a1f14bd3e0c9563615eddfdbfd71d37229
SMFahimShahrear <s.m.shahrear.fahim@gamil.com>
2022-11-04 22:17:10 +0600

    bug fix

--- script.js
+++ script.js
@@ -1,251 +1,57 @@
-let password_show_checkbox = document.querySelector("#checkbox-show-pass");
-let fname_field = document.querySelector(".f-name");
-let lname_field = document.querySelector(".l-name");
-let mail_field = document.querySelector(".mail");
-let password_field = document.querySelector(".password");
-let confirm_password_field = document.querySelector(".confirm-password");
-let error = 0;
-let data = "";
-let buttonCnt = 0;
-let sliderCnt = 0;
-let mail_field2 = '';
+let sliderCnt = 1;
+let Signin_Signup_Cnt = 1;
 
-function checkfuntion(e) {
-    if (e.value != "") {
-        e.nextElementSibling.classList.add('input-filled');
+function submitButton(e){
+    if(sliderCnt == 1){
+    document.querySelector(".frame-box").classList.add("scale-small");
+    document.querySelector(".section-wrapper").classList.add("transition-left-first");
+    document.querySelector(".button-wrapper").classList.add("button-wrapper-fix");
+    document.querySelector(".frame-above").classList.add("button-wrapper-fix");
+    document.querySelector(".google-logo-image img").classList.add("transition-google-logo");
+    document.querySelector(".title-signin-email").classList.add("transition-google-logo");
+    document.querySelector(".title-signup").classList.add("hidden");
+    document.querySelector(".title-signin-email").classList.remove("hidden");
+    document.querySelector(".subtitle").classList.add("hidden");
+    sliderCnt++;
     }
-    else {
-        e.nextElementSibling.classList.remove('input-filled');
-    }
-}
-
-function password_visibility(e) {
-    if (e.checked == true) {
-        password_field.type = "text";
-        e.parentElement.classList.add("check-box-hover-color");
-    } else {
-        password_field.type = "password";
-        e.parentElement.classList.remove("check-box-hover-color");
-    }
-}
-function password_visibility2(e) {
-    if (e.checked == true) {
-        document.querySelector(".pass-check").type = "text";
-        e.parentElement.classList.add("check-box-hover-color");
-    } else {
-        document.querySelector(".pass-check").type = "password";
-        e.parentElement.classList.remove("check-box-hover-color");
-    }
-}
-
-// validation
-function validation() {
-
-    if (buttonCnt == 0) {
-        // document.querySelectorAll(".empty-field-alart").classList.add("hidden");
-        document.querySelector(".empty-field-alart-1").classList.add("hidden");
-        document.querySelector(".empty-field-alart-2").classList.add("hidden");
-        document.querySelector(".empty-field-alart-3").classList.add("hidden");
-        document.querySelector(".empty-field-alart-4").classList.add("hidden");
-        document.querySelector(".empty-field-alart-5").classList.add("hidden");
-        document.querySelector(".empty-field-alart-6").classList.add("hidden");
-        document.querySelector(".empty-field-alart-7").classList.add("hidden");
-        document.querySelector(".empty-field-alart-8").classList.add("hidden");
-        document.querySelector(".empty-field-alart-9").classList.add("hidden");
-        fname_field.classList.remove("border-red");
-        lname_field.classList.remove("border-red");
-        mail_field.classList.remove("border-red");
-        password_field.classList.remove("border-red");
-        error = 0;
-
-        if (fname_field.value == "" && lname_field.value == "") {
-            fname_field.classList.add("border-red");
-            lname_field.classList.add("border-red");
-            document.querySelector(".empty-field-alart-1").classList.remove("hidden");
-            error = 1;
-        } else if (fname_field.value == "") {
-            fname_field.classList.add("border-red");
-            document.querySelector(".empty-field-alart-2").classList.remove("hidden");
-            error = 1;
-        } else if (lname_field.value == "") {
-            lname_field.classList.add("border-red");
-            document.querySelector(".empty-field-alart-3").classList.remove("hidden");
-            error = 1;
-        }
-
-        if (mail_field.value == "") {
-            mail_field.classList.add("border-red");
-            document.querySelector(".empty-field-alart-4").classList.remove("hidden");
-            document.querySelector(".mail-rule").classList.add("hidden");
-            error = 1;
-        }
-        else {
-            let check = localStorage.getItem(mail_field.value);
-
-            if (check) {
-                sessionStorage.setItem("mailaddress", mail_field);
-                error = 1;
-                document.querySelector(".empty-field-alart-4").classList.add("hidden");
-                document.querySelector(".empty-field-alart-5").classList.remove("hidden");
-            }
-        }
-
-        if (password_field.value == "") {
-            password_field.classList.add("border-red");
-            document.querySelector(".empty-field-alart-6").classList.remove("hidden");
-            document.querySelector(".empty-field-alart-8").classList.add("hidden");
-            document.querySelector(".empty-field-alart-7").classList.add("hidden");
-            document.querySelector(".password-rule").classList.add("hidden");
-            error = 1;
-        }
-
-        else if (password_field.value.length < 7) {
-            password_field.classList.add("border-red");
-            document.querySelector(".empty-field-alart-7").classList.remove("hidden");
-            document.querySelector(".empty-field-alart-8").classList.add("hidden");
-            document.querySelector(".password-rule").classList.add("hidden");
-            error = 1;
-        }
-        else {
-            if (confirm_password_field.value == "") {
-                confirm_password_field.classList.add("border-red");
-                document.querySelector(".empty-field-alart-8").classList.remove("hidden");
-                document.querySelector(".empty-field-alart-7").classList.add("hidden");
-                document.querySelector(".password-rule").classList.add("hidden");
-                error = 1;
-            }
-            else {
-                if (password_field.value.localeCompare(confirm_password_field.value) != 0) {               //passowrd match
-                    error = 1;
-                    document.querySelector(".empty-field-alart-9").classList.remove("hidden");
-                    document.querySelector(".password-rule").classList.add("hidden");
-                }
-            }
-        }
-
-        if (error == 1) {
-            return false;
-        } else {
-            data = password_field.value.length + "pass:" + password_field.value + "fname:" + fname_field.value + "lname:" + lname_field.value + "||";
-            localStorage.setItem(mail_field.value, data);
-            window.alert("Account created!");
-        }
-    }
-
-    else if (buttonCnt == 1) {
-
-        document.querySelector(".empty-field-alart-20").classList.add("hidden");
-        mail_field2 = document.querySelector('.mail-check').value;
-        let check = localStorage.getItem(mail_field2);
-        let position_start = check.search("fname:");
-        let position_end = check.search("lname:");
-        let username = check.slice(position_start + 6, position_end);
-        document.querySelector(".subtitle p").innerText = username.toUpperCase();
-
-        if (mail_field2 == '') {
-            document.querySelector(".empty-field-alart-21").classList.remove("hidden");
-            return false;
-        }
-
-        if (check) {
-            document.querySelector(".wrapper-all").classList.add("scale-static-2");
-            document.querySelector(".main-title p").innerText = 'Welcome';
-            document.querySelector(".main-title p").classList.remove("center-trasition-2");
-            document.querySelector(".main-title p").classList.remove("scale-static-3");
-            document.querySelector(".main-title p").classList.add("center-stick");
-            // document.querySelector(".subtitle p").classList.remove("center-trasition-2");
-            document.querySelector(".subtitle p").classList.remove("scale-static-5");
-            document.querySelector(".subtitle p").classList.add("center-stick");
-
-        } else {
-            document.querySelector(".empty-field-alart-20").classList.remove("hidden");
-            document.querySelector(".empty-field-alart-21").classList.add("hidden");
-            return false;
-        }
+    else if(sliderCnt == 2){
+        document.querySelector(".section-wrapper").classList.add("transition-left-second");
     }
     
-    else if (buttonCnt == 2) {
-        let password = document.querySelector(".pass-check");
-        document.querySelector(".empty-field-alart-30").classList.add("hidden");
-        document.querySelector(".empty-field-alart-31").classList.add("hidden");
-        let find = mail_field2;
-
-        if (password.value == '') {
-            document.querySelector(".empty-field-alart-31").classList.remove("hidden");
-            return false;
-        }
-        if (find) {
-            let str = localStorage.getItem(find);
-            let position_start = str.search("pass:");
-            let position_end = str.search("fname:");
-            let pass = str.slice(position_start + 5, position_end);
-            if (pass.localeCompare(password.value) == 0) {
-                document.querySelector(".main-frame").classList.add("hidden");
-                document.querySelector(".welcome").classList.remove("hidden");
-                return false;
-            }
-            else {
-                document.querySelector(".empty-field-alart-30").classList.remove("hidden");
-                return false;
-            }
-        }
-        else {
-            window.alert("Error occured");
-            return false;
-        }
-    }
-    buttonCnt++;
-    return false;
-
 }
 
 
-function slider1(){
-    // document.querySelector(".google-logo-image img").classList.add("center-stick");
-    if(sliderCnt == 0){
-    document.querySelector(".google-logo-image img").classList.add("center-trasition");
-    document.querySelector(".main-title p").classList.add("center-trasition-2");
-    document.querySelector(".main-frame").classList.add("scale");
-    document.querySelector(".wrapper-all").classList.add("scale-static");
-    document.querySelector(".main-title p").classList.add("scale-static-3");
-    document.querySelector(".subtitle p").classList.add("scale-static-5");
-    document.querySelector(".google-logo-image img").classList.add("scale-static-3");
-    document.querySelector(".submit-wrapper div").classList.add("scale-static-5");
-    document.querySelector(".submit-wrapper button").classList.add("scale-static-4");
-    document.querySelector(".main-title p").innerText = 'Sign in';
-    document.querySelector(".subtitle p").innerText = 'Use your Google Account';
-    buttonCnt = 1;
-    sliderCnt = 1;
-    }
+function checkfuntion(e){
 
-    else if(sliderCnt == 1){
-        document.querySelector(".google-logo-image img").classList.remove("center-trasition");
-        document.querySelector(".main-frame").classList.remove("scale");
-        document.querySelector(".wrapper-all").classList.remove("scale-static");
-        document.querySelector(".main-title p").innerText = 'Create your Google Account';
-        document.querySelector(".main-title p").classList.remove("scale-static-3");
-        document.querySelector(".main-title p").classList.remove("center-trasition-2");
-        document.querySelector(".subtitle p").classList.remove("scale-static-5");
-        document.querySelector(".google-logo-image img").classList.remove("scale-static-3");
-        document.querySelector(".submit-wrapper div").classList.remove("scale-static-5");
-        document.querySelector(".submit-wrapper button").classList.remove("scale-static-4");
-        document.querySelector(".subtitle p").innerText = ' ';
-        buttonCnt = 0;
-        sliderCnt = 0;
-    }
-}
-function nameappear() {
-
-    if (find) {
-        let str = localStorage.getItem(mail_field2);
-        let position_start = str.search("fname:");
-        let position_end = str.search("lname:");
-        let username = str.slice(position_start + 6, position_end);
-        document.querySelector(".subtitle p").innerText = username.toUpperCase();
-    }
 }
 
-function passAuth() {
+function signin_signup(){
+    console.log(Signin_Signup_Cnt);
+    if(Signin_Signup_Cnt == 1){
+        document.querySelector(".frame-box").classList.add("scale-small");
+        document.querySelector(".section-wrapper").classList.add("transition-left-first");
+        document.querySelector(".button-wrapper").classList.add("button-wrapper-fix");
+        document.querySelector(".frame-above").classList.add("button-wrapper-fix");
+        document.querySelector(".google-logo-image img").classList.add("transition-google-logo");
+        document.querySelector(".title-signin-email").classList.add("transition-google-logo");
+        document.querySelector(".title-signup").classList.add("hidden");
+        document.querySelector(".title-signin-email").classList.remove("hidden");
+        document.querySelector(".subtitle").classList.add("hidden");
+        Signin_Signup_Cnt++;
+    } 
+       
+    else if(Signin_Signup_Cnt == 2){
+        Signin_Signup_Cnt--;
+        document.querySelector(".frame-box").classList.remove("scale-small");
+        document.querySelector(".section-wrapper").classList.remove("transition-left-first");
+        document.querySelector(".button-wrapper").classList.remove("button-wrapper-fix");
 
-
+        document.querySelector(".frame-above").classList.remove("button-wrapper-fix");
+        document.querySelector(".google-logo-image img").classList.remove("transition-google-logo");
+        document.querySelector(".title-signin-email").classList.remove("transition-google-logo");
+        document.querySelector(".title-signup").classList.remove("hidden");
+        document.querySelector(".title-signin-email").classList.add("hidden");
+        document.querySelector(".subtitle").classList.remove("hidden");
+        signin_signup
+    }
 }
